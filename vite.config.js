e838f63379c14256729f13e56db4aea1c54313dc
Collin Jester <jestercollin@gmail.com>
2026-05-19 15:53:30 -0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: ['es2015', 'safari13'],
-    outDir: 'dist',
-  },
-  esbuild: {
-    target: 'es2015',
-  },
 })
